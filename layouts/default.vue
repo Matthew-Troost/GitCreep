4fb08001cdcfcbbf64555b831b6a69b8e05a3903
matt_thetechmillion <matt@thetechmillion.com>
2020-05-09 12:25:54 +0200

    Started creating the footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,38 +6,34 @@
       </div>
     </section>
     <footer class="footer">
-      <div class="content has-text-centered">
-        <p>
-          <strong>Bulma</strong> by
-          <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is
-          licensed
-          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The
-          website content is licensed
-          <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
-            >CC BY NC SA 4.0</a
-          >.
-        </p>
+      <div class="columns">
+        <div class="column is-two-thirds">
+          Donate
+        </div>
+        <div class="column">
+          <a href="https://github.com/Matthew-Troost/gitcreep" target="_blank">
+            <img :src="githubLogo" class="_github" />
+            <small>@Matthew-Troost</small>
+          </a>
+          <hr />
+          <a href="https://heroku.com" target="_blank">
+            <img :src="herokuLogo" class="_heroku" />
+          </a>
+        </div>
       </div>
     </footer>
   </div>
 </template>
 
 <script>
+import herokuLogo from '@/assets/images/built-on-heroku-light.svg'
+import githubLogo from '@/assets/images/GitHub-Mark-Light.png'
+
 export default {
   data() {
     return {
-      items: [
-        {
-          title: 'Home',
-          icon: 'home',
-          to: { name: 'index' }
-        },
-        {
-          title: 'Inspire',
-          icon: 'lightbulb',
-          to: { name: 'inspire' }
-        }
-      ]
+      herokuLogo,
+      githubLogo
     }
   }
 }
@@ -50,5 +46,18 @@ export default {
 .footer {
   background-color: #222;
   color: #9aa1aa;
+}
+.footer .columns {
+  max-width: 1000px;
+  margin: auto;
+}
+._heroku {
+  max-width: 250px;
+}
+._github {
+  max-width: 32px;
+}
+hr {
+  background-color: #32363b;
 }
 </style>
